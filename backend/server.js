12aa8be140e1bc27d51c856bc90d82e36166138a
minahil <minahilrehmat00@gmail.com>
2026-03-09 17:04:21 +0500

    done

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // 2. Frontend Serving Logic
-const frontendDist = path.join(process.cwd(), "frontend", "dist");
-
+const frontendDist = path.join(__dirname, "../frontend/dist");
 // Debugging log jo Vercel dashboard mein nazar aaye ga
 console.log("Looking for frontend at:", frontendDist);
 
